4b33528d4a0be1614ee2dd3ed33d849902eb5095
epredator <trojnaradam@gmail.com>
2015-05-30 23:26:13 +0200

    dodanie do utworzonych w pakiecie .resources dwóch klas: MessageResource oraz ProgramResource dodanie po dwie funkcje GET oraz POST

--- radioPlan/src/main/java/com/etroya/resources/MessageResource.java
+++ radioPlan/src/main/java/com/etroya/resources/MessageResource.java
@@ -1,7 +1,28 @@
 package com.etroya.resources;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 /**
  * Created by Adam on 2015-05-30.
  */
+
+@Path("/messages")
 public class MessageResource {
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getMessages(){
+        return "this is new message.";
+    }
+
+
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    public String postMessage(String message){
+        return "Twoja wiadomość: "+ message;
+
+    }
 }
--- radioPlan/src/main/java/com/etroya/resources/ProgramResource.java
+++ radioPlan/src/main/java/com/etroya/resources/ProgramResource.java
@@ -1,7 +1,28 @@
 package com.etroya.resources;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 /**
  * Created by Adam on 2015-05-30.
  */
+
+@Path("/programs")
 public class ProgramResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getProgram(){
+        return "To jest Twój program";
+    }
+
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    public String postProgram(String programName){
+        return "Nazwa programu: "+programName;
+    }
+
 }

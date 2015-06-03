ba67ba8ab24c497ae93e7d91cb5aa40ad0adbe09
epredator <trojnaradam@gmail.com>
2015-06-03 11:16:45 +0200

    stworzenie klasy Profile w paczce Model, która ma za zadanie reprezentować profile użytkownika. Rozpoczęcie implementacji mockupu bazy danych

---
+++ radioPlan/src/main/java/com/etroya/database/DatabaseClass.java
@@ -0,0 +1,17 @@
+package com.etroya.database;
+
+import com.etroya.model.Message;
+import com.etroya.model.Profile;
+import com.etroya.model.Program;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Adam on 2015-06-03.
+ */
+public class DatabaseClass {
+    private static Map<Long, Message> messages = new HashMap<>();
+    private static Map<Long, Profile> profiles = new HashMap<>();
+    private static Map<Long, Program> programs = new HashMap<>();
+}
---
+++ radioPlan/src/main/java/com/etroya/model/Profile.java
@@ -0,0 +1,54 @@
+package com.etroya.model;
+
+/**
+ * Created by Adam on 2015-06-03.
+ */
+public class Profile {
+    private long id;
+    private String profileName;
+    private String firstName;
+    private String lastName;
+
+    public Profile(){
+
+    }
+
+    public Profile(long id, String profileName, String firstName, String lastName){
+        this.id = id;
+        this.profileName = profileName;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getProfileName() {
+        return profileName;
+    }
+
+    public void setProfileName(String profileName) {
+        this.profileName = profileName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}

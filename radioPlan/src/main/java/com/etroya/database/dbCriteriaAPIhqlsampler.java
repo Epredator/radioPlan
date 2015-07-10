60dbbc7e8472c72165e51890efc9a931d78f63a3
epredator <trojnaradam@gmail.com>
2015-07-10 09:28:43 +0400

    wykorzystanie Projekcji do pobierania wybranych danych a nie całego obiektu z DB, Restriction.or (Hibernate, PostgreSQL)

--- radioPlan/src/main/java/com/etroya/database/dbCriteriaAPIhqlsampler.java
+++ radioPlan/src/main/java/com/etroya/database/dbCriteriaAPIhqlsampler.java
@@ -5,6 +5,7 @@ import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
 
 import java.util.List;
@@ -17,12 +18,21 @@ public class dbCriteriaAPIhqlsampler {
         SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
         Session session = sessionFactory.openSession();
         session.beginTransaction();
-        Criteria criteria = session.createCriteria(Profile.class);
-        criteria.add(Restrictions.or(
-                Restrictions.like("profileName", "%Botlowski%"),
-                Restrictions.gt("id", 5L)
-                )
-        );
+//        Criteria criteria = session.createCriteria(Profile.class);
+//        criteria.add(Restrictions.or(
+//                Restrictions.like("profileName", "%Botlowski%"),
+//                Restrictions.gt("id", 5L)
+//                )
+//        );
+//        Criteria criteria = session.createCriteria(Profile.class)
+//                .setProjection(Projections.property("userId")); //pobieranie jedynie kolumny które chcemy a nie całego obiektu
+
+//        Criteria criteria = session.createCriteria(Profile.class)
+//                .setProjection(Projections.max("userId")); //pobieranie jedynie kolumny które chcemy a nie całego obiektu
+
+        Criteria criteria = session.createCriteria(Profile.class)
+                .setProjection(Projections.count("userId")); //pobieranie jedynie kolumny które chcemy a nie całego obiektu
+
 
         List<Profile> profiles = (List<Profile>) criteria.list();
         session.getTransaction().commit();

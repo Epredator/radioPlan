7f373de03723fb2efb42ea70627db0cb13f5bbda
Adam <trojnaradam@gmail.com>
2017-07-23 21:38:45 +0200

    added findAll  and countAll JPQL transactions

--- radioplan-back/src/main/java/com/etroya/radioplan/repository/StationRepository.java
+++ radioplan-back/src/main/java/com/etroya/radioplan/repository/StationRepository.java
@@ -1,8 +1,12 @@
 package com.etroya.radioplan.repository;
 
+import com.etroya.radioplan.model.Station;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import java.awt.print.Book;
+import java.util.List;
 
 /**
  * Created by eperedator on 22.07.17.
@@ -11,6 +15,7 @@ public class StationRepository {
 
     @PersistenceContext(unitName = "stationStorePU")
     EntityManager em;
+
 
     public Book find(Long id){
         return em.find(Book.class, id);
@@ -27,5 +32,15 @@ public class StationRepository {
     public void delete(Long id){
         em.remove(em.getReference(Book.class, id));
 
+    }
+
+    public List<Station> findAll(){
+        TypedQuery<Station> query = em.createQuery("SELECT s FROM Station s ORDER BY s.title DESC", Station.class);
+        return query.getResultList();
+    }
+
+    public Long countAll(){
+        TypedQuery<Long> query = em.createQuery("SELECT COUNT(s) FROM Station s", Long.class);
+        return query.getSingleResult();
     }
 }

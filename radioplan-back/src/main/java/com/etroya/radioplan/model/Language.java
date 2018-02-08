f103c803856f07764e7adaa03f6330e9d5ae68ab
epredator <trojnaradam@gmail.com>
2018-02-08 23:19:39 +0100

    some html content. Started new structure of project

--- radioplan-back/src/main/java/com/etroya/radioplan/model/Language.java
+++
@@ -1,9 +0,0 @@
-package com.etroya.radioplan.model;
-
-/**
- * Created by eperedator on 22.07.17.
- */
-public enum Language {
-
-    POLISH, ENGLISH, GERMAN
-}
--- src/main/java/pl/etroya/MainApp.java
+++ src/main/java/pl/etroya/MainApp.java
@@ -1,6 +1,6 @@
 package pl.etroya;
 
-
+@SpringBootApplication
 public class MainApp {
     public static void main(String[] args){
         System.out.println("Hello radioplan!");
